0fbfd15db515b152baa7d3ad6eebac641261250c
Maria Rita <125027496+MariaRita011@users.noreply.github.com>
2023-11-27 17:04:17 -0300

    feat: filtro por copa

--- app/mapasMK/page.jsx
+++ app/mapasMK/page.jsx
@@ -4,44 +4,64 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+    const [dados, setDados] = useState([]);
+    const [mapas, setMapas] = useState([]);
+    const [copa, setCopa] = useState('');
+    const [filtrados, setFiltrados] = useState([]);
 
-  const [mapas, setMapas] = useState([]);
-  const [dados, setDados] = ([])
+    useEffect(() => {
+        async function fetchMaps() {
+            try {
+                const response = await axios.get("/api/mapas");
+                setDados(response.data.listaMapas);
+                setMapas(response.data.listaMapas);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
 
-  useEffect(() => {
-    async function fetchMaps() {
-      try {
-        const response = await axios.get("/api/mapas");
-        setMapas(response.data.listaMapas);
-        setDados(response.data.listaMapas);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
+        fetchMaps();
+    }, []);
 
-    fetchMaps();
-  }, []);
+    const aplicarFiltro = (copa) => {
+        const mapasFiltrados = dados.filter((item) => item.copa === copa);
+        setFiltrados(mapasFiltrados);
+        setCopa(copa);
+    };
 
-  console.log("Mapas");
-  console.log(mapas);
+    useEffect(() => {
+            aplicarFiltro(copa);
+    }, [copa]);
 
-  return (
-    <main >
-        <Link href="/mapasMK/cadastro">
-          <button >
-            Cadastrar Aluno
-          </button>
-        </Link>
-      {mapas ? (
-        mapas.map((mapa) => (
-          <div key={mapa.id}>
-            <h1>{mapa.nome}</h1>
-            <img src={mapa.imagem} width={200} height={100}/>
-          </div>
-        ))
-      ) : (
-        <h1 className={style.h1}>Esperando Dados</h1>
-      )}
-    </main>
-  )
+    return (
+        <main>
+            <Link href="/mapasMK/cadastro">
+                <button>
+                    Cadastrar Aluno
+                </button>
+            </Link> 
+            <button onClick={() => aplicarFiltro("Copa Flor")}>Flor</button>
+            <button onClick={() => aplicarFiltro("Copa Casco")}>Casco</button>
+            <button onClick={() => aplicarFiltro("Copa Seta")}>Seta</button>
+            <button onClick={() => aplicarFiltro("Copa Estrela")}>Estrela</button>
+            <button onClick={() => aplicarFiltro("Copa Flor de Cerejeira")}>Flor de Cerejeira</button>
+            <button onClick={() => aplicarFiltro("Copa Banana")}>Banana</button>
+            <button onClick={() => aplicarFiltro("Copa Folha")}>Folha</button>
+            <button onClick={() => aplicarFiltro("Copa Especial")}>Especial</button>
+            <button onClick={() => aplicarFiltro("Copa Ovo")}>Ovo</button>
+            <button onClick={() => aplicarFiltro("Copa Leve")}>Leve</button>
+            <button onClick={() => aplicarFiltro("Copa Flor de Ouro")}>Flor de Ouro</button>
+            <button onClick={() => aplicarFiltro("Copa Coroa")}>Coroa</button>
+            {filtrados.length != 0 ? (
+                filtrados.map((mapa) => (
+                    <div key={mapa.id}>
+                        <h1>{mapa.nome}</h1>
+                        <img src={mapa.imagem} width={200} height={100} />
+                    </div>
+                ))
+            ) : (
+                <h1>Selecione a copa</h1>
+            )}
+        </main>
+    );
 }
