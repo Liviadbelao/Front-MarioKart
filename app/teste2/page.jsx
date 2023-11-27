0935771e1743b0c493956fd4b2ec8cb3fed7890f
manuxcamargo <123407445+manuxcamargo@users.noreply.github.com>
2023-11-27 16:34:26 -0300

    header lateral

---
+++ app/teste2/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderDefault from '../components/header/Header'
+
+function Teste() {
+  return (
+    <div><HeaderDefault/></div>
+  )
+}
+
+export default Teste
