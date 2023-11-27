089f5345bd1c7f7a6b17e7a13207ad648880ae93
Maria Rita <125027496+MariaRita011@users.noreply.github.com>
2023-11-27 08:35:07 -0300

    feat: mostrando na tela

---
+++ app/api/mapas/route.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+import { NextResponse } from "next/server";
+
+const url = process.env.BASE_URL + "mapas";
+
+export async function GET() {
+  try {
+    const response = await axios.get(url);
+
+    console.log("API")
+    console.log(response.data);
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log("[ORDER_GET]", error);
+    return new NextResponse("Erro interno do servidor!", { status: 500 });
+  }
+}
+
+export async function POST(request) {
+  const params = await request.json();
+
+  try {
+    const response = await axios.post(url, params);
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log("[ORDER_POST]", error);
+    return new NextResponse("Erro interno do servidor!", { status: 500 });
+  }
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,40 @@
-
+'use client'
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [mapas, setMapas] = useState([]);
+  const [dados, setDados] = ([])
+
+  useEffect(() => {
+    async function fetchMaps() {
+      try {
+        const response = await axios.get("/api/mapas");
+        setMapas(response.data.listaMapas);
+        setDados(response.data.listaMapas);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchMaps();
+  }, []);
+
+  console.log("Mapas");
+  console.log(mapas);
+
   return (
     <main >
-    
+      {mapas ? (
+        mapas.map((mapa) => (
+          <div key={mapa.id}>
+            <h1>{mapa.id}</h1>
+          </div>
+        ))
+      ) : (
+        <h1>Esperando Dados</h1>
+      )}
     </main>
   )
 }
