adb125c513fa7286323f04f990265efa700dd0a3
Maria Rita Gomes <mariagomes@aluno.senai.br>
2023-12-07 20:54:11 -0300

    feat: responsividade

--- app/components/botoescopas/BotoesCopas.jsx
+++ app/components/botoescopas/BotoesCopas.jsx
@@ -2,11 +2,11 @@ import styles from "./botoescopa.module.css"
 const BotoesCopas = ({ imagem, oc, copaSelecionada, aplicarFiltro}) => {
 
   return (
-    <div className={styles.botoesContainer}>
+    <p>
                         <button className={`${styles.botoes} ${copaSelecionada === oc ? styles.botoesSelecionado : ''}`} onClick={() => aplicarFiltro(oc)}>
                             <img className={styles.imagecup} src={imagem} width={150} height={150} />
                         </button>
-                    </div>
+    </p>
 )
 }
 export default BotoesCopas
