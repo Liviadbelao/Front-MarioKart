d435be8f9c63b89e9f80810780fa9e36d08d149b
mariacancian <mariacancian.564@gmail.com>
2023-12-08 10:05:24 -0300

    feat: modalSobre

--- app/api/usuarios/route.js
+++ app/api/usuarios/route.js
@@ -9,8 +9,8 @@ export async function GET() {
   try {
     const response = await axios.get(url);
 
-    console.log("API")
-    console.log(response.data);
+   /*  console.log("API")
+    console.log(response.data); */
 
     return NextResponse.json(response.data);
   } catch (error) {
