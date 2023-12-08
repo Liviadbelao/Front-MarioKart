572b446ad796edb689eb15f9bbdfe1e1ee210924
Giulia Marques <giuliamarques.564@gmail.com>
2023-12-08 17:04:59 -0300

    feat create: css cadastro

--- app/components/inputs/Inputs.jsx
+++ app/components/inputs/Inputs.jsx
@@ -1,7 +1,11 @@
+import styles from "./inputs.module.css"
+
 const Inputs = ({ tipo, valor, pl, oc}) => {
 
   return (
-    <input type={tipo} value={valor} placeholder={pl} onChange={oc} />
+    <div className={styles.containerInputs}>
+    <input type={tipo} value={valor} placeholder={pl} onChange={oc} className={styles.Inputs} />
+    </div>
 )
 }
 export default Inputs
