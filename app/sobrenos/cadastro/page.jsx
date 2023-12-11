dfddadbd0974719382cf1e12a924b367b799e206
Maria Rita <125027496+MariaRita011@users.noreply.github.com>
2023-12-11 17:14:18 -0300

    feat:comentarios pt4

--- app/sobrenos/cadastro/page.jsx
+++ app/sobrenos/cadastro/page.jsx
@@ -1,4 +1,6 @@
 "use client";
+
+//Importações
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -6,7 +8,10 @@ import Image from "next/image";
 import Inputs from "@/app/components/inputs/Inputs";
 import styles from "./cadastro.module.css"
 
+//Criação da página
 export default function Cadastro() {
+
+  //Criando estados
   const [nome, setNome] = useState("");
   const [erroNome, setErroNome] = useState("");
   const [idade, setIdade] = useState("");
@@ -17,97 +22,106 @@ export default function Cadastro() {
   const [imagem, setImagem] = useState("");
   const [erroImagem, setErroImagem] = useState("");
   const [avatar, setAvatar] = useState("");
-
   const [usuarios, setUsuarios] = useState([]);
-  let erros = [];
 
+  //Imagem válida
   const urlValida = (imagem) => {
     if (imagem.match(/\.(jpeg|jpg|gif|png)$/) != null) {
-        return true;
+      return true;
     } else {
-        return false;
+      return false;
     }
   }
 
-
+  //Criando nova pessoa
   const adicionar = async (e) => {
     e.preventDefault();
 
-    console.log(nome, avatar, idade, descricao, tipo, imagem )
-
-    if (nome == '') {
+    //Verificações
+    if (!nome) {
       setErroNome('Preencha o campo Nome');
-  } else if (nome.length < 3 || nome.length >20) {
+    } else if (nome.length < 3 || nome.length > 20) {
       setErroNome('O tamanho do nome deve ser entre 3 a 20 caracteres')
-  }else {
+    } else {
       setErroNome('');
-  }
+    }
 
-  if(!imagem) {
-    console.log('Preencha o campo imagem')
-    setErroImagem('Preencha o campo Imagem')
-} else if (!urlValida(imagem)) {
-    console.log('A imagem precisa ser valida')
-    setErroImagem('A imagem precisa ter um formato válido: .jpeg/.jpg/.gif/.png')
-} else {
-    console.log('Limpou');
-    setErroImagem('');
-}
+    if (!imagem) {
+      console.log('Preencha o campo imagem')
+      setErroImagem('Preencha o campo Imagem')
+    } else if (!urlValida(imagem)) {
+      console.log('A imagem precisa ser valida')
+      setErroImagem('A imagem precisa ter um formato válido: .jpeg/.jpg/.gif/.png')
+    } else {
+      console.log('Limpou');
+      setErroImagem('');
+    }
 
-if(descricao == '') {
-  setErroDescricao('Preencha o campo Descrição')
- }else if(descricao.length < 10 || descricao.length > 100) {
-     setErroDescricao('O tamanho da descrição deve ser entre 10 a 100 caracteres')
- } else {
-     setErroDescricao('');
- }
+    if (!descricao) {
+      setErroDescricao('Preencha o campo Descrição')
+    } else if (descricao.length < 10 || descricao.length > 100) {
+      setErroDescricao('O tamanho da descrição deve ser entre 10 a 100 caracteres')
+    } else {
+      setErroDescricao('');
+    }
 
- if(idade == '') {
-  setErroIdade('Preencha o campo idade')
- }else if(idade < 13) {
-     setErroIdade('O usuário deve ser maior de 13 anos.')
- } else {
-     setErroIdade('');
- }
+    if (!idade) {
+      setErroIdade('Preencha o campo idade')
+    } else if (idade < 13) {
+      setErroIdade('O usuário deve ser maior de 13 anos.')
+    } else {
+      setErroIdade('');
+    }
 
     try {
-      await axios.post("/api/usuarios", { nome, avatar, idade, descricao, tipo, imagem  });
+      await axios.post("/api/usuarios", { nome, avatar, idade, descricao, tipo, imagem });
       setNome("");
       setAvatar("");
       setIdade("");
       setDescricao("");
       setTipo("");
       setImagem("");
-     
+
     } catch (error) {
       console.error("Error submitting data:", error);
     }
+
   };
 
+  //Coletando dados da API
   useEffect(() => {
+
     async function buscarUsuarios() {
+
       try {
         const resposta = await axios.get("/api/usuarios");
         setUsuarios(resposta.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
+
     }
+
     buscarUsuarios();
+
   }, []);
 
+  //Select avatar
   const handleAvatarChange = (e) => {
     const selectedAvatar = e.target.value;
     setAvatar(selectedAvatar);
   };
 
+  //Select tipo
   const handleTypeUser = (e) => {
     const selectedTypeUser = e.target.value;
     setTipo(selectedTypeUser);
   };
 
+  //Criando HTML
   return (
     <div className={styles.main}>
+      
       <div>
         <Link href="/sobrenos">
           <button className={styles.btn_hibrido}>Mostrar usuários</button>
@@ -117,22 +131,16 @@ if(descricao == '') {
       <div className={styles.container}>
         <h1>Cadastrar Usuário</h1>
         <form onSubmit={adicionar} className={styles.mainContainer}>
+
           <div>
             <label htmlFor="name" className={styles.label}> Nome </label>
-            <Inputs tipo={'text'} valor={nome} oc={(e) => setNome(e.target.value)}/>
+            <Inputs tipo={'text'} valor={nome} oc={(e) => setNome(e.target.value)} />
             <p>{erroNome}</p>
           </div>
 
           <div>
             <label htmlFor="avatar" className={styles.label}> Selecione seu avatar</label>
-            <select
-              name="avatar"
-              id="avatar"
-              onChange={handleAvatarChange}
-              value={avatar} className={styles.select}
-            >
-              {/* <option value="caminho/para/imagem1.jpg">Imagem 1</option>
-              <option value="caminho/para/imagem2.jpg">Imagem 2</option> */}
+            <select name="avatar" id="avatar" onChange={handleAvatarChange} value={avatar} className={styles.select}>
               <option value=''>Selecione...</option>
               <option value="/cadastroSobreNos/abelha_rainha-removebg-preview.png">Abelha Rainha</option>
               <option value="/cadastroSobreNos/baby_pitty-removebg-preview.png">Baby Pitty</option>
@@ -177,11 +185,8 @@ if(descricao == '') {
               <option value="/cadastroSobreNos/tartaruga de lacinho-PhotoRoom.png-PhotoRoom.png">Wendy</option>
               <option value="/cadastroSobreNos/teen monkey-PhotoRoom.png-PhotoRoom.png">Diddy kong</option>
               <option value="/cadastroSobreNos/veio maluco-PhotoRoom.png-PhotoRoom.png">Professor E.Gadd</option>
-
-
-              
-              {/* Adicione mais opções conforme necessário */}
             </select>
+
             {avatar && (
               <Image
                 src={avatar}
@@ -195,34 +200,39 @@ if(descricao == '') {
 
           <div>
             <label htmlFor="idade" className={styles.label}> Idade</label>
-            <Inputs tipo={'number'} valor={idade} oc={(e) => setIdade(e.target.value)}/>
+            <Inputs tipo={'number'} valor={idade} oc={(e) => setIdade(e.target.value)} />
             <p>{erroIdade}</p>
           </div>
+
           <div>
             <label htmlFor="descricao" className={styles.label}> Descrição</label>
-            <Inputs tipo={"text"} valor={descricao} oc={(e) => setDescricao(e.target.value)}/>
-           <p> {erroDescricao}</p>
+            <Inputs tipo={"text"} valor={descricao} oc={(e) => setDescricao(e.target.value)} />
+            <p> {erroDescricao}</p>
           </div>
+
           <div>
             <label htmlFor="tipo" className={styles.label}> Selecione seu tipo de usuário</label>
-            <select  type="text" id="tipo" value={tipo} onChange={handleTypeUser} className={styles.select}>
+            <select type="text" id="tipo" value={tipo} onChange={handleTypeUser} className={styles.select}>
               <option value="Aluno"> Selecione... </option>
               <option value="Aluno"> Aluno </option>
               <option value="Instrutor"> Instrutor </option>
               <option value="Vistante"> Visitante </option>
             </select>
           </div>
+
           <div>
             <label htmlFor="imagem" className={styles.label}> Imagem</label>
-            <Inputs tipo={"text"} valor={imagem} oc={(e) => setImagem(e.target.value)}/>
-              <p>{erroImagem}</p>
+            <Inputs tipo={"text"} valor={imagem} oc={(e) => setImagem(e.target.value)} />
+            <p>{erroImagem}</p>
           </div>
 
-        
-
           <button type="submit" className={styles.button}>cadastrar</button>
+          
         </form>
+
       </div>
+
     </div>
+
   );
 }
