4f70ebf206ddce6a1cc191886a7393b3f295e1cd
MariaCancian <maria.cancian@aluno.senai.br>
2023-12-14 15:09:22 -0300

    feat: header nova

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -4,47 +4,46 @@ import NavFooter from '../navfooter/NavFooter';
 const Header = () => {
     return (
 
-        <div>
-        <img src={"/membros/c03790eb-7c50-4196-9b8e-20fccae209af.jpg"}  height={60} className={styles.caixinha} />
+         
+
+            <div className={styles.header}>
+
+                <div className={styles.links}>
 
 
-        <div className={styles.header}>
 
-            <div className={styles.links}>
-
-
-                <div className={styles.linksContainer}>
-                    <div className={styles.mario}>
-                        <img src={"/pagHome/icon-mario.png.png"} width={40} height={40} />
+                    <div className={styles.img}>
+                        <img src={"/membros/74d4f295-8121-4959-b08a-04508f938c2a.png"} width={200} height={100} />
                     </div>
-                    <NavFooter className={styles.linksNavPages} rota={'/mapasMK'} texto={'Mapa'} />
-                </div>
+                    <div className={styles.linksContainer}>
+                        <div className={styles.linkagem}>
 
-                <div className={styles.linksContainer}>
-                    <div className={styles.mario}>
-                        <img src={"/pagHome/icon-mario.png.png"} width={40} height={40} />
+                            <NavFooter className={styles.linksNavPages} rota={'/mapasMK'} texto={'Mapa'} />
+
+                        </div>
+
+                        <div className={styles.linkagem}>
+
+                            <NavFooter className={styles.linksNavPages} rota={'/sobrenos'} texto={'Sobre Nós'} />
+
+                        </div>
+
+                        <div className={styles.linkagem}>
+                            <NavFooter className={styles.linksNavPages} rota={'/mapasMK/cadastro'} texto={'Cadastro'} />
+
+                        </div>
+
+                        <div className={styles.linkagem}>
+
+                            <NavFooter className={styles.linksNavPages} rota={'/contato'} texto={'Contato'} />
+
+                        </div>
+
                     </div>
-                    <NavFooter className={styles.linksNavPages} rota={'/mapasMK/cadastro'} texto={'Cadastro'} />
                 </div>
+                </div> 
+                );
+                
+            }
 
-                <div className={styles.linksContainer}>
-                    <div className={styles.mario}>
-                        <img src={"/pagHome/icon-mario.png.png"} width={40} height={40} />
-                    </div>
-                    <NavFooter className={styles.linksNavPages} rota={'/sobrenos'} texto={'Sobre Nós'} />
-                </div>
-
-                <div className={styles.linksContainer}>
-                    <div className={styles.mario}>
-                        <img src={"/pagHome/icon-mario.png.png"} width={40} height={40} />
-                    </div>
-                    <NavFooter className={styles.linksNavPages} rota={'/contato'} texto={'Contato'} />
-                </div>
-
-            </div>
-        </div>
-        </div>
-    );
-}
-
-export default Header
+                export default Header
