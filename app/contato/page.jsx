c79ff1041056a55baca733930c8017477cefce32
Maria Rita Gomes <mariagomes@aluno.senai.br>
2023-12-14 22:26:58 -0300

    feat: verificações e componentes

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -4,22 +4,59 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import style from "./cadastro/cadastro.module.css";
-
-import Link from "next/link";
 import TrocarTela from "@/app/components/trocartela/TrocarTela";
 import Inputs from "@/app/components/inputs/Inputs";
 import Label from "@/app/components/label/label";
+import Header from "../components/header/Header";
 
 export default function Register() {
     const [nome, setNome] = useState("");
+    const [nomeErro, setNomeErro] = useState("");
     const [email, setEmail] = useState("");
+    const [emailErro, setEmailErro] = useState("");
     const [telefone, setTelefone] = useState("");
+    const [telefoneErro, setTelefoneErro] = useState("");
     const [mensagem, setMensagem] = useState("");
+    const [mensagemErro, setMensagemErro] = useState("");
     const [contato, setContato] = useState([]);
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (!nome) {
+            setNomeErro('Preencha o campo Nome');
+          } else if (nome.length < 3 || nome.length > 20) {
+            setNomeErro('O tamanho do nome deve ser entre 3 a 20 caracteres')
+          }else {
+            setNomeErro('')
+          }
+        
+          if (!email) {
+            setEmailErro('Preencha o campo Email')
+          } else if (!isEmailValid(email)) {
+            setEmailErro('O email precisa conter "@"')
+          }else {
+            setEmailErro('')
+          }
+        
+          if (!telefone) {
+            setTelefoneErro('Preencha o campo Telefone');
+          }else if (isNaN(telefone)) {
+            setTelefoneErro('O telefone deve conter apenas números');
+          } else if (telefone.length != 11) {
+            setTelefoneErro('O telefone deve conter 11 dígitos');
+          }else {
+            setTelefoneErro('')
+          }
+        
+          if (!mensagem) {
+            setMensagemErro('Preencha o campo Mensagem')
+          } else if( mensagem.length > 150) {
+            setMensagemErro("A mensagem é muito longa")
+          }else {
+            setMensagemErro('')
+          }
 
         try {
             await axios.post("/api/contato", { nome, email, telefone, mensagem });
@@ -33,6 +70,11 @@ export default function Register() {
             console.error("Error submitting data:", error);
         }
     };
+
+    function isEmailValid(email) {
+        return email.includes('@');
+      }
+      
 
     useEffect(() => {
         async function fetchContato() {
@@ -48,6 +90,7 @@ export default function Register() {
     }, []);
     return (
         <div className={style.container}>
+            <Header />
 
             <div className={style.parallax}>
 
@@ -62,21 +105,24 @@ export default function Register() {
 
                         <form onSubmit={handleSubmit}>
                             <div>
-                                <Label htmlFor={'nome'} />
-                                <label className={style.label} htmlFor="name">Nome:</label>
+                                <Label htmlFor={'nome'} texto={'Nome:'} />
                                 <Inputs tipo={'text'} valor={nome} oc={(e) => setNome(e.target.value)} />
+                                {nomeErro}
                             </div>
                             <div>
-                                <label className={style.label} htmlFor="name">E-mail:</label>
+                            <Label htmlFor={'email'} texto={'Email:'} />
                                 <Inputs tipo={'text'} valor={email} oc={(e) => setEmail(e.target.value)} />
+                                {emailErro}
                             </div>
                             <div>
-                                <label className={style.label} htmlFor="name">Telefone:</label>
+                            <Label htmlFor={'telefone'} texto={'Telefone:'} />
                                 <Inputs tipo={'text'} valor={telefone} oc={(e) => setTelefone(e.target.value)} />
+                                {telefoneErro}
                             </div>
                             <div>
-                                <label className={style.label} htmlFor="name">Mensagem:</label>
+                            <Label htmlFor={'mensagem'} texto={'Mensagem:'} />
                             <Inputs tipo={'text'} valor={mensagem} oc={(e) => setMensagem(e.target.value)} />
+                            {mensagemErro}
                     </div>
 
                     <button className={style.button2} type="submit">Cadastrar</button>
